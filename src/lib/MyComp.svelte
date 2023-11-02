b1e8d65fe8b6ca468b829941c17b62543eacb19b
Rene Rubalcava <rrubalcava@esri.com>
2023-11-02 16:22:20 -0700

    basic sveltekit demo

---
+++ src/lib/MyComp.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { onMount } from 'svelte';
+	import '@esri/calcite-components/dist/calcite/calcite.css';
+
+	onMount(async () => {
+		console.log('mounted');
+		const { setAssetPath } = await import('@esri/calcite-components/dist/components');
+		setAssetPath('https://js.arcgis.com/calcite-components/1.10.0/assets');
+		await import('@esri/calcite-components/dist/components/calcite-button');
+	});
+</script>
+
+<calcite-button on:click={() => console.log('calcite button click')}>My Button</calcite-button>
+
+<style>
+</style>
